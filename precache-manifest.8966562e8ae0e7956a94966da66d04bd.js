3990f706f94555da8d98c56e3fa30ca1046abd00
joseg23 <00051716@uca.edu.sv>
2019-11-01 11:48:01 -0600

    Updates

---
+++ precache-manifest.8966562e8ae0e7956a94966da66d04bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416b8d212a2e20368a8c87e6bac75e19",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "ac0594febfbbed9c259b",
+    "url": "/TicTacToe/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/TicTacToe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "ac0594febfbbed9c259b",
+    "url": "/TicTacToe/static/js/main.31d36c1c.chunk.js"
+  },
+  {
+    "revision": "664d87082df4062f2db9",
+    "url": "/TicTacToe/static/js/runtime-main.326ff5c5.js"
+  }
+]);
